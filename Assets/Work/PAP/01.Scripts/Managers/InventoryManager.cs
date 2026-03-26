f02fd675a4dc9827cec65c9f3a58c45b6443a929
pap <projectpaper11@gmail.com>
2026-03-26 22:28:35 +0900

    abc1

---
+++ Assets/Work/PAP/01.Scripts/Managers/InventoryManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager instance;
+
+    
+    
+    private void Awake()
+    {
+        instance = this;
+    }
+}
