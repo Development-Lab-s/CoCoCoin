bea39059d9b9c16392847e719da77f83a737d0d9
pap <projectpaper11@gmail.com>
2026-04-01 17:12:17 +0900

    a
    a

--- Assets/Work/PAP/01.Scripts/Inventory.cs
+++ Assets/Work/PAP/01.Scripts/Inventory.cs
@@ -1,6 +1,14 @@
+using NUnit.Framework;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    
+    static public List<Chip> instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = 
+        }
+    }
 }
