0a71f6c1d30af67147eb532238ed3cbad334ed64
Hex48 <hexual48@gmail.com>
2026-04-02 14:24:47 +0900

    Change : CoinFlipAnim
    26.04.02
    14.30

--- Assets/00. Work/mori/02. Scripts/Flip.cs
+++ Assets/00. Work/mori/02. Scripts/Flip.cs
@@ -5,6 +5,7 @@ public class Flip : MonoBehaviour
     private Animator anim;
     private Rigidbody2D rb;
     private float jumpForce = 15f;
+    public Animator HandAnim;
 
     void Start()
     {
@@ -14,12 +15,15 @@ public class Flip : MonoBehaviour
     
     void OnMouseDown()
     {
+        rb.gravityScale = 3;
+        HandAnim.SetBool("isFlipping", true);
         anim.SetBool("isFlip", true);
         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
+        HandAnim.SetBool("isFlipping", false);
         anim.SetBool("isFlip", false);
+        rb.gravityScale = 0;
     }
 }
