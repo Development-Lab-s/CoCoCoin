7c28fa2d30ff24e7600a3bcece7780cab1356126
bchera <zkdhtm1330@gmail.com>
2026-04-08 18:37:20 +0900

    peristalsis
    inventorysystem,lootingsystem peristalsis
    2026/04/08
    pm 6:37

--- Assets/00. Work/leesamuel/01.Scripts/NormalCoin_Control.cs
+++ Assets/00. Work/leesamuel/01.Scripts/NormalCoin_Control.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class NormalCoin_Control : MonoBehaviour
 {
-    public string _Coin_Name = "Normal_Coin";
+    [SerializeField] private InventoryItemSO so;
 
     private float _Size_Change_Scale = 0.001f;//크기 변환 속도
     private bool _Select=false;//선택되지 않음으로 시작
@@ -39,12 +39,12 @@ public class NormalCoin_Control : MonoBehaviour
         if (_Select == true)
         {
             _Select = false;
-            CoinManager.Cansle_Select(_Coin_Name);
+            CoinManager.Cansle_Select(so);
             //선택한 코인갯수 줄이는 메서드 작동
         }
         else
         {
-            bool A = CoinManager.Select(_Coin_Name);//최대 선택갯수가 아니라 선택이 가능하면 true반환 아니면false반환
+            bool A = CoinManager.Coin_Select(so);//최대 선택갯수가 아니라 선택이 가능하면 true반환 아니면false반환
             if (A == false)
             {
                 //나아아아중에 UI로 선택 최대치입니다 띄우기.
--- Assets/00. Work/leesamuel/01.Scripts/Select.cs
+++ Assets/00. Work/leesamuel/01.Scripts/Select.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class Select : MonoBehaviour
 {
-    private CoinManager CoinManager;
+    private CoinManager coinManager;
     private void Start()
     {
-        CoinManager = GameObject.Find("CoinManager").GetComponent<CoinManager>();
+        coinManager = GameObject.Find("CoinManager").GetComponent<CoinManager>();
     }
     private void OnMouseDown()
     {
-        
+        if (coinManager.select_a != null)
+            coinManager.Select(coinManager.select_a);
+        if(coinManager.select_b != null)
+            coinManager.Select(coinManager.select_b);
+        if(coinManager.select_c != null)
+            coinManager.Select(coinManager.select_c);
     }
 }
