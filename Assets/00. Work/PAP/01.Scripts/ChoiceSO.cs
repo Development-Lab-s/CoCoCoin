e6407d065b686f0c4f54bc779ed13b090ca5ab53
pap <projectpaper11@gmail.com>
2026-05-14 19:49:26 +0900

    a
    a

---
+++ Assets/00. Work/PAP/01.Scripts/ChoiceSO.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace _00._Work.PAP._01.Scripts
+{
+    [CreateAssetMenu(fileName = "Choice data", menuName = "SO/Choice data", order = 0)]
+    public class ChoiceSO : ScriptableObject
+    {
+        [TextArea(10,50)] public string choiceText;
+        public Sprite choiceImage;
+        public string[] choiceButtons;
+    }
+}
