1d6439e324b3685788f772f1e5b70d0f17a49169
당끼 <dangkki0928@gmail.com>
2026-05-20 20:29:50 +0900

    feat : shop#12
    26.5.20
    8.29

--- Assets/00. Work/dangkki/01. Scipts/ItemManager.cs
+++ Assets/00. Work/dangkki/01. Scipts/ItemManager.cs
@@ -16,8 +16,10 @@ public class ItemManager : MonoBehaviour
     public InventoryUI inventoryUI;
     
     public InventorySO _chipList;
+    public InventorySO _healthList;
     public InventorySO _inventory;
     public Transform[] _itemList;
+    public Transform _healthBox;
     public Descripton _descripton;
     public InventoryItemSO[] _sellitem;
     
@@ -27,6 +29,8 @@ public class ItemManager : MonoBehaviour
     private Image[] _itemList_SoldOut;
     private SpriteLibrary[] _itemList_Spin;
     private EventTrigger[] _itemList_EventTrigger;
+    
+    public InventoryItemSO _healthChip = null;
     
     string[] _sellList;
     bool[] _isSold;
@@ -41,7 +45,7 @@ public class ItemManager : MonoBehaviour
         _itemList_EventTrigger = new EventTrigger[_itemList.Length];
         _itemList_Spin = new SpriteLibrary[_itemList.Length];
         _sellitem = new InventoryItemSO[_itemList.Length];
-        _isSold = new bool[_itemList.Length];
+        _isSold = new bool[_itemList.Length + 1];
         for (int i = 0; i < _itemList.Length; i++)
         {
             _itemList_Sprite[i] = _itemList[i].GetChild(0).GetComponent<Image>();
@@ -53,10 +57,12 @@ public class ItemManager : MonoBehaviour
             _itemList_SoldOut[i].enabled = false;
             _itemList_Sprite[i].color = Color.white;
         }
+        
     }
     private void Start()
     {
         SetItem();
+        SetHealth();
     }
     
     public void SetItem()
@@ -96,9 +102,18 @@ public class ItemManager : MonoBehaviour
             _itemList_Sprite[i].sprite = ChosenChip.Sprite;
             _itemList_Spin[i].spriteLibraryAsset = ChosenChip.spriteLibrary;
             //_itemList_Animation[i].runtimeAnimatorController = _chipList.inventoryItemList[randomNum].SelectSprite;
-            if(ChosenChip.rarity == InventoryItemSO.Rarity.Common) _itemList_Price[i].text = "3$";
-            else if(ChosenChip.rarity == InventoryItemSO.Rarity.Rare) _itemList_Price[i].text = "5$";
-            else if(ChosenChip.rarity == InventoryItemSO.Rarity.Legendary) _itemList_Price[i].text = "10$";
+            if(ChosenChip.rarity == InventoryItemSO.Rarity.Common) {
+                _itemList_Price[i].text = "3$";
+                _itemList_Price[i].color = Color.white;
+            }
+            else if(ChosenChip.rarity == InventoryItemSO.Rarity.Rare) {
+                _itemList_Price[i].text = "5$";
+                _itemList_Price[i].color = Color.green;
+            }
+            else if(ChosenChip.rarity == InventoryItemSO.Rarity.Legendary) {
+                _itemList_Price[i].text = "10$";
+                _itemList_Price[i].color = Color.yellow;
+            }
             //_itemList_Price[i].text = _chipList.ShopItemList[randomNum].Description;
             _sellitem[i] = ChosenChip;
             //_descripton.SetDescription(i);
@@ -163,4 +178,90 @@ public class ItemManager : MonoBehaviour
         _itemList_Price[itemNumber - 1].text = "Sold Out";
         _isSold[itemNumber - 1] = true;
     }
+
+    public void SetHealth()
+    {
+            _healthBox.GetChild(0).GetChild(0).GetComponent<Image>().enabled = false;
+            int randomNum = Random.Range(0, 10);
+            if (randomNum <= 5)
+            {
+                _healthChip = _healthList.inventoryItemList[0];
+                _healthBox.GetChild(1).GetComponent<TextMeshProUGUI>().text = "5$";
+                _healthBox.GetChild(1).GetComponent<TextMeshProUGUI>().color = Color.white;
+            }
+            else if (randomNum > 5 && randomNum <= 9)
+            {
+                _healthChip = _healthList.inventoryItemList[1];
+                _healthBox.GetChild(1).GetComponent<TextMeshProUGUI>().text = "10$";
+                _healthBox.GetChild(1).GetComponent<TextMeshProUGUI>().color = Color.green;
+            }
+            else
+            {
+                _healthChip = _healthList.inventoryItemList[2];
+                _healthBox.GetChild(1).GetComponent<TextMeshProUGUI>().text = "25$";
+                _healthBox.GetChild(1).GetComponent<TextMeshProUGUI>().color = Color.yellow;
+            }
+            _healthBox.GetChild(0).GetComponent<Image>().sprite = _healthChip.Sprite;
+            _healthBox.GetChild(0).GetComponent<Image>().color = Color.white;
+
+        
+    }
+    public void BuyHealth()
+    {
+        if (_isSold[5] == true)
+        {
+            Debug.Log("이미 구매한 아이템입니다.");
+            return;
+        }
+        if (_healthChip.rarity == InventoryItemSO.Rarity.Common)
+        {
+            if (_ticketManager._ticket >= 5)
+            {
+                _ticketManager._ticket -= 5;
+                GameData.instance.playerCurrentHp += 25;
+            }
+            else
+            {
+                Debug.Log("현제 티켓 : " + _ticketManager._ticket + " 티켓이 부족합니다.");
+                return;
+            }
+
+        }
+        else if (_healthChip.rarity == InventoryItemSO.Rarity.Rare)
+        {
+            if (_ticketManager._ticket >= 10)
+            {
+                _ticketManager._ticket -= 10;
+                GameData.instance.playerCurrentHp += 50;
+            }
+            else
+            {
+                Debug.Log("현제 티켓 : " + _ticketManager._ticket + " 티켓이 부족합니다.");
+                return;
+            }
+        }
+        else if (_healthChip.rarity == InventoryItemSO.Rarity.Legendary)
+        {
+            if (_ticketManager._ticket >= 25)
+            {
+                _ticketManager._ticket -= 25;
+                GameData.instance.playerCurrentHp += 100;
+            }
+            else
+            {
+                Debug.Log("현제 티켓 : " + _ticketManager._ticket + " 티켓이 부족합니다.");
+                return;
+            }
+                
+        }
+        _ticketManager.SetTicketText();
+        inventoryUI.ScrollView();
+        GameData.instance.playerCurrentHp = Mathf.Clamp(GameData.instance.playerCurrentHp, 0, 100);
+        Debug.Log("체력 구매완료");
+        _healthBox.GetChild(0).GetChild(0).GetComponent<Image>().enabled = true;
+        _healthBox.GetChild(0).GetComponent<Image>().color = Color.gray;
+        _healthBox.GetChild(1).GetComponent<TextMeshProUGUI>().text = "Sold Out";
+        _isSold[5] = true;
+    }
+    
 }
