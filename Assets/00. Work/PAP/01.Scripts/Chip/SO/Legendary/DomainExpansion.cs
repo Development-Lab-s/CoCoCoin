4fb130ac47f08c23233ab1746166f9c88cfe37ef
pap <projectpaper11@gmail.com>
2026-05-21 21:55:52 +0900

    asfd
    asfd

--- Assets/00. Work/PAP/01.Scripts/Chip/SO/Legendary/DomainExpansion.cs
+++ Assets/00. Work/PAP/01.Scripts/Chip/SO/Legendary/DomainExpansion.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using _00._Work.PAP._01.Scripts;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "DomainExpansion", menuName = "ChipEncounterSO/DomainExpansion")]
@@ -9,12 +11,18 @@ public class DomainExpansion : ChipEncounter
     public override void HeadChip(Player player, Enemy enemy)
     {
         Instantiate(effect, Vector3.zero, Quaternion.identity);
-        DamageHandler.CalculateDamage(player,enemy,BattleManager.instance.nowChips.Count * 20);
+        CoroutineRunner.instance.StartCoroutine(StartDamage(player, enemy, BattleManager.instance.nowChips.Count * 20));
         List<InventoryItemSO> a = new List<InventoryItemSO>(BattleManager.instance.nowChips);
         foreach (InventoryItemSO item in a)
         {
             BattleManager.instance.DiscardChip(item,BattleManager.instance.chipModels[(BattleManager.instance.nowChips.IndexOf(item))]);
         }
+    }
+
+    IEnumerator StartDamage(Player player,Enemy enemy,int damage)
+    {
+        yield return new WaitForSeconds(0.5f);
+        DamageHandler.CalculateDamage(player,enemy,damage);
     }
 
     public override void TailChip(Player player, Enemy enemy)
