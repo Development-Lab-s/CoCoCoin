a872e1d5bb0265b9ddd1bd9db05d1567469d04ac
pap <projectpaper11@gmail.com>
2026-05-22 00:49:31 +0900

    final fixed
    a

--- Assets/00. Work/PAP/01.Scripts/Chip/SO/Legendary/BbangHuman.cs
+++ Assets/00. Work/PAP/01.Scripts/Chip/SO/Legendary/BbangHuman.cs
@@ -1,19 +1,17 @@
 using System;
+using System.Collections;
+using _00._Work.PAP._01.Scripts;
 using UnityEngine;
 using Random = System.Random;
 
 [CreateAssetMenu(fileName = "BbangHuman", menuName = "ChipEncounterSO/BbangHuman")]
 public class BbangHuman : ChipEncounter
 {
+    [SerializeField] private GameObject effect;
     public override void HeadChip(Player player, Enemy enemy)
     {
-        BattleManager.instance.isLocked = true;
-        BattleManager.instance.isReplicated = true;
-        BattleManager.instance.needDiscard += Mathf.Clamp(1,0, BattleManager.instance.nowChips.Count);
-        if (BattleManager.instance.needDiscard <= 0)
-        {
-            BattleManager.instance.isLocked = false;
-        }
+        Instantiate(effect, Vector3.zero, Quaternion.identity);
+        CoroutineRunner.instance.StartCoroutine(StartDamage());
     }
 
     public override void TailChip(Player player, Enemy enemy)
@@ -27,6 +25,18 @@ public class BbangHuman : ChipEncounter
                 BattleManager.instance.drawChips.Remove(item);
                 BattleManager.instance.inventory.inventoryItemList.Remove(item);
             }
+        }
+    }
+
+    IEnumerator StartDamage()
+    {
+        yield return new WaitForSeconds(0.5f);
+        BattleManager.instance.isLocked = true;
+        BattleManager.instance.isReplicated = true;
+        BattleManager.instance.needDiscard += Mathf.Clamp(1,0, BattleManager.instance.nowChips.Count);
+        if (BattleManager.instance.needDiscard <= 0)
+        {
+            BattleManager.instance.isLocked = false;
         }
     }
 }
