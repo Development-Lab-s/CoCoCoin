43d5e4201d9a41e71d55122226f9db0988266e40
pap <projectpaper11@gmail.com>
2026-05-24 17:07:23 +0900

    asdf
    asdf

--- Assets/00. Work/PAP/01.Scripts/Chip/SO/Legendary/Vibecoding.cs
+++ Assets/00. Work/PAP/01.Scripts/Chip/SO/Legendary/Vibecoding.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Vibecoding", menuName = "ChipEncounterSO/Vibecoding")]
 public class Vibecoding : ChipEncounter
 {
+    [SerializeField] private GameObject effectObj;
 
     public override void HeadChip(Player player, Enemy enemy)
     {
+        Instantiate(effectObj, Vector3.zero, Quaternion.identity);
         DamageMultiplyStay effect = new DamageMultiplyStay();
         player.statusEffectHandler.AddStatusEffect(effect,1);
     }
 
     public override void TailChip(Player player, Enemy enemy)
     {
+        Instantiate(effectObj, Vector3.zero, Quaternion.identity);
         HalfDamageMultiplyStay effect = new HalfDamageMultiplyStay();
         player.statusEffectHandler.AddStatusEffect(effect,1);
     }
