d08a8a4dee03759e9fdf177f72f0413397edab95
pap <projectpaper11@gmail.com>
2026-05-24 16:16:11 +0900

    asd
    s

--- Assets/00. Work/PAP/01.Scripts/Chip/SO/Legendary/FingerSnap.cs
+++ Assets/00. Work/PAP/01.Scripts/Chip/SO/Legendary/FingerSnap.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "FingerSnap", menuName = "ChipEncounterSO/FingerSnap")]
 public class FingerSnap : ChipEncounter
 {
+    [SerializeField] private GameObject effect;
 
     public override void HeadChip(Player player, Enemy enemy)
     {
+        Instantiate(effect, Vector3.zero, Quaternion.identity);
         enemy.TakeDamage(enemy.EnemyCurrentHP()/2);
     }
 
     public override void TailChip(Player player, Enemy enemy)
     {
+        Instantiate(effect, Vector3.zero, Quaternion.identity);
         player.TakeDamage(GameData.instance.playerCurrentHp/2);
     }
 }
